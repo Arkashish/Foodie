8616667730288c5a7d4d1ab086f8b18269ea85f3
Arkashish Sutradhar <arkashishdhar1@gmail.com>
2025-10-23 20:53:02 +0530

    Removed Scret values

--- foodapp-client/src/firebase/firebase.config.js
+++ foodapp-client/src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDboA8edCFDrKmDcX-bKijkZ6SklcYuguQ",
-  authDomain: "desiqna-foodapp.firebaseapp.com",
-  projectId: "desiqna-foodapp",
-  storageBucket: "desiqna-foodapp.appspot.com",
-  messagingSenderId: "323792364745",
-  appId: "1:323792364745:web:19b62b0a9879063f661918",
+  apiKey: process.env.REACT_APP_APIKEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_APPID,
 };
 
 // Initialize Firebase
